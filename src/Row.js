69228fdf2f879bf5310223df3be5edcfe8a90988
Ali Shahrez <shahrez375@gmail.com>
2025-08-03 21:18:49 +0500

    Initial commit: Set up React project with Netflix clone layout, created Row component, integrated Axios with TMDB API, and implemented basic movie fetching functionality.

---
+++ src/Row.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import './Row.css'
+import axios from './axios';
+const base_url = 'https://image.tmdb.org/t/p/original/'
+
+function Row({ title, fetchURL }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchURL);
+            setMovies(request.data.results);
+            console.table(request)
+        }
+        fetchData();
+    }, [fetchURL]);
+
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row_posters'>
+                {movies.map(movie => (
+                    <img className='row_poster'
+                        key={movie.id} 
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name || movie.title || movie.original_name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Row;
