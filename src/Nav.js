42360bfdf5ff99d9af5b388e3e5653a576a68143
Ali Shahrez <shahrez375@gmail.com>
2025-08-04 00:30:29 +0500

    Design nav-bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, setShow] = useState(false); // start transparent
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShow(true); // show black nav
+      } else {
+        setShow(false); // hide black nav
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show ? "nav_black" : ""}`}>
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
       />
-
       <img
         className="nav_avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
