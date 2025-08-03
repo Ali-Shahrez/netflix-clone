e40ea15cc0c92bbc9997970333c106aef1771c8d
Ali Shahrez <shahrez375@gmail.com>
2025-08-03 21:40:08 +0500

    Add all movie category rows

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <div className="App">
       <Row title='NETFLIX ORIGINALS' fetchURL = {requests.fetchNetflixOriginals}/>
       <Row title = 'TRENDING NOW' fetchURL = {requests.fetchTrending}/>
+      <Row title = 'TOP RATED' fetchURL = {requests.fetchTopRated}/>
+      <Row title = 'ACTION MOVIES' fetchURL = {requests.fetchActionMovies}/>
+      <Row title = 'COMEDY MOVIES' fetchURL = {requests.fetchComedyMovies}/>
+      <Row title = 'HORROR MOVES' fetchURL = {requests.fetchHorrorMovies}/>
+      <Row title = 'ROMANCE MOVIES' fetchURL = {requests.fetchRomaceMovies}/>
+      <Row title = 'DOCUMENTARIES' fetchURL = {requests.fetchDocumentaries}/>
 
     </div>
   );
