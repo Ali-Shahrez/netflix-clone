55db4637e85e3c1f546dab4964221fdaa0753cbe
Ali Shahrez <shahrez375@gmail.com>
2025-08-03 23:37:23 +0500

    Build banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import './Banner.css'
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -16,19 +17,29 @@ function Banner() {
         }
         fetchData();
     }, []);
+    
+function truncate (str, n) {
+    return str?.length > n ? str.substr(0, n-1) + "..." : str;
+}
 
     return (
-        <header className="banner"
-            style = {{
-                backgroundSize:'cover',
-                backgroundImage: `url ("https://tmdb.org/t/p/original/${movie?.backdrop_path}"
-                )`,
-            backgorundPosition: "center center",
-        }}
-            >
+        <header
+            className="banner"
+            style={{
+                backgroundSize: 'cover',
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+            }}
+        >
             <div className="banner_contents">
-            {/* You can display movie title here to test */}
-            <h1>{movie?.name || movie?.title || movie?.original_name}</h1>
+                <h1 className="banner_title">{movie?.name || movie?.title || movie?.original_name}</h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
         </header>
     );
