279cdbf92441682353e18cfb562412ebc93b4acb
Ali Shahrez <shahrez375@gmail.com>
2025-08-07 11:13:02 +0500

    Add a TV Shows page

--- src/pages/TVShows.js
+++ src/pages/TVShows.js
@@ -1,15 +1,22 @@
 import React from "react";
-import Nav from "../components/Nav";
-import "./TVShows.css";
+import './Landing.css'; // reuse same styling
+import Row from '../components/Row';
+import requests from '../requests'; 
+import Banner from '../components/Banner';
+import Nav from '../components/Nav';
 
 function TVShows({ userName }) {
   return (
-    <div className="tvshows">
+    <div className="App">
       <Nav userName={userName} />
-      <div className="tvshows_content">
-        <h1>TV Shows</h1>
-        <p>Browse your favorite shows here.</p>
-      </div>
+      <Banner />
+      <Row title='Only on Netflix' fetchURL={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title='Award-Winning TV Shows' fetchURL={requests.fetchAwardWinningTV} />
+      <Row title='Critically Acclaimed TV Shows' fetchURL={requests.fetchTopRatedTV} />
+      <Row title='US TV Dramas' fetchURL={requests.fetchUSTV} />
+      <Row title='International TV Dramas' fetchURL={requests.fetchInternationalTV} />
+      <Row title='K-Dramas' fetchURL={requests.fetchKDrama} />
+      <Row title='Award-Winning Suspenseful TV Shows' fetchURL={requests.fetchSuspenseTV} />
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -9,6 +9,13 @@ const requests = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomaceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+    fetchAwardWinningTV: `/discover/tv?api_key=${API_KEY}&with_keywords=6075`, // Award-winning
+    fetchTopRatedTV: `/tv/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchUSTV: `/discover/tv?api_key=${API_KEY}&with_origin_country=US`,
+    fetchInternationalTV: `/discover/tv?api_key=${API_KEY}&with_original_language=ko|ja|es|fr`,
+    fetchKDrama: `/discover/tv?api_key=${API_KEY}&with_original_language=ko`,
+    fetchSuspenseTV: `/discover/tv?api_key=${API_KEY}&with_genres=9648&vote_average.gte=7`, // Suspenseful & rated
 };
 
 export default requests;
